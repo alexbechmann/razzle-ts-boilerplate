cc4db48f6a1bef267466b3a06245d3dee36988f0
Alex Bechmann <alex.bechmann@outlook.com>
2019-01-03 00:06:28 +0100

    added config to help IE

--- razzle.config.js
+++ razzle.config.js
@@ -1,15 +1,42 @@
 'use strict';
 
+const babelEnvDeps = require('webpack-babel-env-deps');
+
 module.exports = {
   plugins: ['typescript'],
   modify(config, { target, dev }, webpack) {
-    const appConfig = config; // stay immutable here
+    const appConfig = config;
     appConfig.module.rules.push({
       test: /\.(js|jsx|ts|tsx)?$/,
       loader: 'prettier-loader',
       exclude: /node_modules/
     })
-
+    if (target === 'web') {
+      appConfig.module.rules.push({
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: [
+          babelEnvDeps.exclude()
+        ],
+        options: {
+          babelrc: false,
+          presets: [
+            [
+              'env',
+              {
+                "modules": "commonjs",
+                "targets": {
+                  "browsers": ["last 2 versions", "ie >= 11"]
+                }
+              }
+            ]
+          ],
+          plugins: [
+            "transform-es2015-arrow-functions",
+          ]
+        }
+      })
+    }
     return appConfig;
   },
 };
