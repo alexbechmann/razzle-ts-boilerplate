3d40f205a4b8b206f8b67cba27a014bfc7b37353
Alex Bechmann <alex.bechmann@outlook.com>
2019-03-29 23:08:16 +0100

    refactored folder & razzle config

--- razzle.config.js
+++ razzle.config.js
@@ -1,49 +1,82 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const babelEnvDeps = require('webpack-babel-env-deps');
+const path = require("path");
+const babelEnvDeps = require("webpack-babel-env-deps");
+const typescript = require("razzle-plugin-typescript");
+const OpenBrowserPlugin = require("open-browser-webpack-plugin");
 
 module.exports = {
-  plugins: ['typescript'],
-  modify(config, { target, dev }, webpack) {
-    const appConfig = config;
-    if (target === 'web') {
+  plugins: [],
+  modify(config, args, webpack) {
+    const { target, dev } = args;
+    let appConfig = config;
+    if (target === "web") {
       appConfig.module.rules = [
         ...appConfig.module.rules,
         {
           test: /\.(js|jsx)$/,
-          loader: 'ts-loader',
-          exclude: [
-            babelEnvDeps.exclude()
-          ],
-          options: {
-            transpileOnly: true
-          }
-        },
-        {
-          test: /\.(js|jsx|ts|tsx)$/,
-          loader: 'ts-loader',
-          include: [
-            path.resolve(__dirname, 'api'),
-            path.resolve(__dirname, 'workplace-core'),
-            path.resolve(__dirname, 'umbraco-headless'),
-            path.resolve(__dirname, 'news'),
-            path.resolve(__dirname, 'workplace-styles'),
-          ],
+          loader: "ts-loader",
+          exclude: [babelEnvDeps.exclude()],
           options: {
             transpileOnly: true
           }
         }
-      ]
+      ];
+      appConfig = typescript(config, args, webpack, {
+        useBabel: false,
+        useEslint: false,
+        tsLoader: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+          configFile: path.resolve(__dirname, "tsconfig.json")
+        },
+        forkTsChecker: {
+          tsconfig: path.resolve(__dirname, "tsconfig.json"),
+          tslint: "./tslint.json",
+          watch: "./src",
+          typeCheck: true,
+          tslintAutoFix: dev
+        }
+      });
+    } else {
+      if (!dev) {
+        appConfig.optimization = {
+          ...appConfig.optimization,
+          minimize: false
+        };
+        appConfig.externals = [];
+      }
+      appConfig = typescript(config, args, webpack, {
+        useBabel: false,
+        useEslint: false,
+        tsLoader: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+          configFile: path.resolve(__dirname, "tsconfig.server.json")
+        },
+        forkTsChecker: {
+          tsconfig: path.resolve(__dirname, "tsconfig.server.json"),
+          tslint: "./tslint.json",
+          watch: ["./src"],
+          typeCheck: true,
+          tslintAutoFix: dev
+        }
+      });
+    }
+    if (dev && target === "web") {
+      appConfig.plugins = [
+        ...appConfig.plugins,
+        new OpenBrowserPlugin({ url: "http://localhost:3000" })
+      ];
     }
     appConfig.module.rules = [
       ...appConfig.module.rules,
       {
         test: /\.(js|jsx|ts|tsx|css|scss)$/,
-        loader: 'prettier-loader',
-        exclude: /node_modules/,
+        loader: "prettier-loader",
+        exclude: /node_modules/
       }
-    ]
+    ];
     return appConfig;
-  },
+  }
 };
